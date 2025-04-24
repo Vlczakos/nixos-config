ad56dd2f39150c3c819425e1aec5869ac746c4cf
Vlczakos <david@vlcak.com>
2025-04-24 20:12:09 +0200

    krakatice init

--- hosts/krakatice/configuration.nix
+++ hosts/krakatice/configuration.nix
@@ -24,6 +24,8 @@
   };
   home-manager.users.vlczak.imports = [ ./home.nix ];
 
+  services.openssh.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
