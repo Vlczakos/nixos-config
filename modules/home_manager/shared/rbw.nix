216e9f781e0fcedcf3b9ac4b1d541564688325dd
Vlczakos <david@vlcak.com>
2025-04-25 11:05:58 +0200

    rbw pinentry

--- modules/home_manager/shared/rbw.nix
+++ modules/home_manager/shared/rbw.nix
@@ -1,10 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.rbw = {
     enable = true;
 
     settings = {
       email = "vlcek.david@outlook.cz";
+      pinentry = pkgs.pinentry-tty;
     };
   };
 }
