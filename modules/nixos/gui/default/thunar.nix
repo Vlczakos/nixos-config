5f3fe6b1a33ea67202ed3d784b6d9146fb700066
Vlczakos <david@vlcak.com>
2025-09-13 23:28:48 +0200

    thunar fix

--- modules/nixos/gui/default/thunar.nix
+++ modules/nixos/gui/default/thunar.nix
@@ -1,10 +1,30 @@
 { pkgs, ... }:
+let
+  thunarUnwrapped = pkgs.xfce.thunar.overrideAttrs (o: {
+    configureFlags = (o.configureFlags or []) ++ [ "--disable-wallpaper-plugin" ];
+  });
+
+  thunarWithPlugins = pkgs.callPackage
+    "${builtins.dirOf pkgs.xfce.thunar.meta.position}/wrapper.nix"
+    {
+      inherit (pkgs) makeWrapper symlinkJoin lib;
+      thunarPlugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
+      thunar-unwrapped = thunarUnwrapped;
+    };
+in
 {
-  programs.thunar = {
-    enable = true;
+  environment.systemPackages = [
+    thunarWithPlugins
+  ];
 
-    plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
-  };
+  services.dbus.packages = [
+    thunarWithPlugins
+  ];
 
+  systemd.packages = [
+    thunarWithPlugins
+  ];
+
+  programs.xfconf.enable = true;
   programs.file-roller.enable = true;
 }
