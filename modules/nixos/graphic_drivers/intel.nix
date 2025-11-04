bb0ed42ead6d456d8fa2e07e83d09904825a71ef
Vlczakos <david@vlcak.com>
2025-11-04 10:21:17 +0100

    edit graphics driver

--- modules/nixos/graphic_drivers/intel.nix
+++ modules/nixos/graphic_drivers/intel.nix
@@ -6,9 +6,15 @@
       libglvnd
       mesa
 
+      intel-media-driver
+      intel-vaapi-driver
+      libvdpau-va-gl
+
       vpl-gpu-rt          # for newer GPUs on NixOS >24.05 or unstable
       # onevpl-intel-gpu  # for newer GPUs on NixOS <= 24.05
       # intel-media-sdk   # for older GPUs
     ];
   };
+
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
 }
