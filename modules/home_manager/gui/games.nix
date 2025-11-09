5830a2716015972b6ce34b27f4d4fd203243113c
Vlczakos <david@vlcak.com>
2025-11-09 15:43:13 +0100

    mangohud

--- modules/home_manager/gui/games.nix
+++ modules/home_manager/gui/games.nix
@@ -6,5 +6,18 @@
 
   programs.mangohud = {
     enable = true;
+
+    settings = {
+      cpu_stats = true;
+      cpu_temp = true;
+
+      gpu_stats = true;
+      gpu_temp = true;
+
+      fps = true;
+      frametime = true;
+
+      toggle_hud = "Shift_R";
+    };
   };
 }
