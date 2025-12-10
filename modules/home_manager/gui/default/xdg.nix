686d0c1ca4025b3bb14ba5023a39200bd2613670
Vlczakos <david@vlcak.com>
2025-12-10 23:31:59 +0100

    xdg mime images fix

--- modules/home_manager/gui/default/xdg.nix
+++ modules/home_manager/gui/default/xdg.nix
@@ -1,22 +1,52 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   xdg = {
     enable = true;
     mime.enable = true;
     mimeApps = {
       enable = true;
-      defaultApplications = {
-        "inode/directory" = [ "thunar.desktop" ];
-        "application/zip" = [ "org.gnome.FileRoller.desktop" ];
-        "x-scheme-handler/terminal" = [ "kitty-open.desktop" ];
-        "text/plain" = [ "codium.desktop" ];
-        "image/*" = [ "qimgv.desktop" ];
-      };
+      defaultApplications =
+        let
+          imageSubtypes = [
+            "jpeg"
+            "jpg"
+            "png"
+            "gif"
+            "webp"
+            "bmp"
+            "tiff"
+            "tif"
+            "svg"
+            "ico"
+            "heic"
+            "heif"
+            "avif"
+            "jxl"
+            "raw"
+          ];
+          imageMimes = map (sub: "image/${sub}") imageSubtypes;
+          imageAssoc = lib.listToAttrs (
+            map (mime: {
+              name = mime;
+              value = [ "qimgv.desktop" ];
+            }) imageMimes
+          );
+        in
+        {
+          "inode/directory" = [ "thunar.desktop" ];
+          "application/zip" = [ "org.gnome.FileRoller.desktop" ];
+          "x-scheme-handler/terminal" = [ "kitty-open.desktop" ];
+          "text/plain" = [ "codium.desktop" ];
+        }
+        // imageAssoc;
     };
     portal = {
       enable = true;
-      extraPortals = [pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk];
-      configPackages = [pkgs.hyprland];
+      extraPortals = [
+        pkgs.xdg-desktop-portal-hyprland
+        pkgs.xdg-desktop-portal-gtk
+      ];
+      configPackages = [ pkgs.hyprland ];
     };
     configFile."mimeapps.list".force = true;
   };
